d3f06afaa0cec4d3596d3e7598dbf7882fb5ed49
Ethan <etfo225@gmail.com>
2023-10-15 14:43:13 -0400

    moved business dash to match Kevins

---
+++ pages/partials/business_dash/active_table.php
@@ -0,0 +1,47 @@
+<div class="col-lg-12">
+  <div class="card card-block card-stretch card-height">
+    <div class="card-header d-flex justify-content-between">
+      <div class="iq-header-title">
+        <h4 class="card-title mb-0">Active Products</h4>
+      </div>
+    </div>
+    <div class="card-body">
+      <div class="table-responsive data-table">
+        <table class="data-tables table" style="width:100%">
+          <thead>
+            <tr>
+              <th>Product</th>
+              <th>Count</th>
+              <th>Expiration Date</th>
+              <th>Time Left</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Google Enterprise</td>
+              <td>1,504 Users</td>
+              <td>10/05/2024</td>
+              <td>365 Days</td>
+              <td>
+                <div class="d-flex align-items-center list-action">
+                  <div class="badge bg-primary-light" data-toggle="tooltip" data-placement="top" title="" data-original-title="Action">
+                    <div class="dropdown">
+                      <div class="text-primary dropdown-toggle action-item" id="moreOptions1" data-toggle="dropdown" aria-haspopup="true" role="button" aria-expanded="false">
+
+                      </div>
+                      <div class="dropdown-menu" aria-labelledby="moreOptions1">
+                        <a class="dropdown-item" href="#">Edit</a>
+                        <a class="dropdown-item" href="#">Delete</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
